28848b4491293595186c3d945fd56d517fe7c33c
Max Wilson <wilson.max@gmail.com>
2023-09-09 06:54:41 -0700

    wip on combat setup logic POC

--- src/CageFight/Scratch.fsx
+++ src/CageFight/Scratch.fsx
@@ -2,6 +2,7 @@
 #load @"Core\Packrat.fs"
 #load @"Domain\Random.fs"
 #load @"Domain\Domain.fs"
+open Domain
 open Domain.Random
 let swingDamage st bonusOrPenalty =
     if st < 9 then RollSpec.create(1,6, bonusOrPenalty + (st-12)/2)
@@ -24,7 +25,40 @@ let thrustDamage st bonusOrPenalty =
 for st in [1..40] do
     printfn "%d: %O" st (thrustDamage st 0)
 
-open Domain.Parser
-open Packrat
-match ParseArgs.Init "Bob the Barbarian: ST 14" with
-| Creature(c, End) -> c
+open System
+type Status = OK | Stunned | Prone | Unconscious | Dead
+type Combatant = {
+    personalName: string
+    team: int
+    stats: Creature
+    damageTaken: int
+    statusMods: Status list
+    }
+type Combat = {
+    combatants: Map<string, Combatant>
+    }
+let renderTeam = function
+    | 0 -> "Blue"
+    | 1 -> "Red"
+    | _ -> failwith "Invalid team"
+let toCombatants (db: Map<string, Creature>) team opposition (quantity, name:string) =
+    let includeTeamInName = (opposition |> List.exists (fun (_, name') -> name = name'))
+    [for i in 1..quantity do
+        {   personalName =
+                if quantity > 1 then sprintf "%s %d" name i else name
+                |> fun name -> if includeTeamInName then sprintf "%s %s" (renderTeam team) name else name
+            team = team
+            stats = db[name]
+            damageTaken = 0
+            statusMods = [] }
+        ]
+let createCombat (db: Map<string, Creature>) team1 team2 =
+    { combatants =
+        (team1 |> List.collect (toCombatants db 0 team2)) @ (team2 |> List.collect (toCombatants db 1 team1))
+        |> Seq.map(fun c -> c.personalName, c)
+        |> Map.ofSeq
+        }
+let db = Defaults.database()
+(3, "Orc") |> toCombatants db 0 []
+let combat = createCombat db [ 3, "Orc"; 1, "Slugbeast"; 1, "Skeleton"] [ 14, "Orc" ]
+combat.combatants.Values |> Seq.sortBy(fun c -> c.team, c.personalName) |> Seq.map (fun c -> $"{c.personalName} [{renderTeam c.team}]") |> Seq.iter (printfn "%s")
