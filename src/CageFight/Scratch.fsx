652670a78decadb9d6f4dfa110aae83891b65054
Max Wilson <wilson.max@gmail.com>
2023-09-09 18:31:32 -0700

    Identify target

--- src/CageFight/Scratch.fsx
+++ src/CageFight/Scratch.fsx
@@ -27,26 +27,26 @@ for st in [1..40] do
 
 open System
 type Status = OK | Stunned | Prone | Unconscious | Dead
+type CombatantId = int * string
 type Combatant = {
     personalName: string
+    number: int
     team: int
     stats: Creature
     damageTaken: int
     statusMods: Status list
     }
+    with
+    member this.CurrentHP_ = this.stats.HP_ - this.damageTaken
+    member this.Id : CombatantId = this.team, this.personalName
 type Combat = {
-    combatants: Map<string, Combatant>
+    combatants: Map<CombatantId, Combatant>
     }
-let renderTeam = function
-    | 0 -> "Blue"
-    | 1 -> "Red"
-    | _ -> failwith "Invalid team"
-let toCombatants (db: Map<string, Creature>) team opposition (quantity, name:string) =
-    let includeTeamInName = (opposition |> List.exists (fun (_, name') -> name = name'))
+let toCombatants (db: Map<string, Creature>) team (quantity, name:string) =
     [for i in 1..quantity do
         {   personalName =
                 if quantity > 1 then sprintf "%s %d" name i else name
-                |> fun name -> if includeTeamInName then sprintf "%s %s" (renderTeam team) name else name
+            number = i
             team = team
             stats = db[name]
             damageTaken = 0
@@ -54,11 +54,39 @@ let toCombatants (db: Map<string, Creature>) team opposition (quantity, name:str
         ]
 let createCombat (db: Map<string, Creature>) team1 team2 =
     { combatants =
-        (team1 |> List.collect (toCombatants db 0 team2)) @ (team2 |> List.collect (toCombatants db 1 team1))
-        |> Seq.map(fun c -> c.personalName, c)
+        (team1 |> List.collect (toCombatants db 0)) @ (team2 |> List.collect (toCombatants db 1))
+        |> Seq.map(fun c -> c.Id, c)
         |> Map.ofSeq
         }
 let db = Defaults.database()
-(3, "Orc") |> toCombatants db 0 []
+(3, "Orc") |> toCombatants db 0
 let combat = createCombat db [ 3, "Orc"; 1, "Slugbeast"; 1, "Skeleton"] [ 14, "Orc" ]
-combat.combatants.Values |> Seq.sortBy(fun c -> c.team, c.personalName) |> Seq.map (fun c -> $"{c.personalName} [{renderTeam c.team}]") |> Seq.iter (printfn "%s")
+combat.combatants.Values |> Seq.filter (fun c -> c.team = 0)
+combat.combatants.Values |> Seq.sortBy(fun c -> c.Id) |> Seq.map (fun c -> $"{c.Id}") |> Seq.iter (printfn "%s")
+let tryFindTarget (combat: Combat) (attacker: Combatant) =
+    let betweenInclusive (min, max) x = min <= x && x <= max
+    let potentialTargets =
+        combat.combatants.Values
+        |> Seq.filter (fun c -> c.team <> attacker.team)
+        |> Seq.filter (fun c -> c.statusMods |> List.forall (fun s -> not (s = Dead || s = Unconscious)))
+        // We don't want to overkill damage, so we put targets that might fall unconscious by themselves
+        // fairly low in priority, although we also want to damage vulnerable targets while they're vulnerable
+        // so we put stunned and prone targets at high priority.
+        // prefer targets that are stunned but not yet at -HP,
+        // then targets that are prone but not yet at -HP,
+        // then targets that are below 1/3 HP but not yet at 0 HP,
+        // then targets at or below 0 HP
+        // then anyone still alive (ordered by statblock name and number because why not, and it makes readouts more predictable)
+        |> Seq.sortBy(fun c ->
+            (c.statusMods |> List.exists ((=) Stunned))
+                && c.CurrentHP_ > -c.stats.HP_,
+            (c.statusMods |> List.exists ((=) Prone))
+                && c.CurrentHP_ > -c.stats.HP_,
+            betweenInclusive (0, (c.stats.HP_ + 1) / 3) c.CurrentHP_,
+            c.CurrentHP_ <= 0,
+            c.stats.name,
+            c.number)
+    potentialTargets |> Seq.tryHead
+combat.combatants[(0, "Orc 3")] |> tryFindTarget combat
+for c in combat.combatants.Values |> Seq.sortBy (fun c -> c.team, c.stats.name, c.number) do
+    printfn "%A attacks %A" c.Id (tryFindTarget combat c).Value.Id
